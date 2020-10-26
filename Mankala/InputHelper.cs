4cfd28bbd33f9426279a5d95e83fca63d1294cf5
LRV2K1 <l.r.verhage@hetnet.nl>
2020-10-26 19:05:55 +0100

    Inputhelper

--- Mankala/InputHelper.cs
+++ Mankala/InputHelper.cs
@@ -6,13 +6,6 @@ namespace Mankala
 {
     static class InputHelper
     {
-        public static bool KeyPressed(char c)
-        {
-            ConsoleKeyInfo cki = Console.ReadKey();
-            Console.Write("\b \b");
-            return cki.KeyChar == c;
-        }
-
         public static int GetNumber()
         {
             string number = "";
